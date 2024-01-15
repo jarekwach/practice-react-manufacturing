798dd8998df021941ddb6a1a0c1d49754752703f
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2024-01-15 17:56:11 +0100

    task-01 done

---
+++ src/01/Task01.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Task01 = () => {
+	return (
+		<div>
+			<h2>Task01</h2>
+			<p>
+				Tablica Trello:{' '}
+				<a
+					href='https://trello.com/b/OrzW82Xb/practice-react-manufacturing'
+					target='blank'>
+					practice-react-manufacturing
+				</a>
+			</p>
+		</div>
+	);
+};
+
+export default Task01;
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Task01 from './01/Task01';
+
+function App() {
+	return (
+		<div>
+			<Task01 />
+		</div>
+	);
+}
+
+export default App;
