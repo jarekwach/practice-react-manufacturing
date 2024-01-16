1ce7ad3ed0d6ee855740b42e32122198d496f7d2
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2024-01-16 13:06:28 +0100

    add task04

---
+++ src/04/Task04.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Task04 = () => {
+	return (
+		<div>
+			<h2>Task04</h2>
+			<p>
+				<a
+					href='https://netlify.com/'
+					target='blank'>
+					netlify
+				</a>
+			</p>
+		</div>
+	);
+};
+
+export default Task04;
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Task01 from './01/Task01';
 import Task02 from './02/Task02';
 import Task03 from './03/Task03';
+import Task04 from './04/Task04';
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<Task01 />
 			<Task02 />
 			<Task03 />
+			<Task04 />
 		</div>
 	);
 }
