ec13cb3255636c35599d64cdd31dec0708443e39
Jaroslaw Wachala <jaroslaw.wachala@gmail.com>
2024-01-16 11:45:25 +0100

    add deploy on github pages

---
+++ src/03/Task03.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Task03 = () => {
+	return (
+		<div>
+			<h2>Task03</h2>
+			<p>
+				<a
+					href='https://jarekwach.github.io/practice-react-manufacturing/'
+					target='blank'>
+					gh-pages
+				</a>
+			</p>
+		</div>
+	);
+};
+
+export default Task03;
